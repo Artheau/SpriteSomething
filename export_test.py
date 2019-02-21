be30f307a57e4110e88b70b77607d42d03c24c46
Artheau <isidian@gmail.com>
2019-02-20 18:48:51 -0800

    remove debug code

--- export_test.py
+++ export_test.py
@@ -6,11 +6,9 @@ import util
 data = util.Samus()
 
 
-# for animation_number in range(len(data.animations)):
-# 	if data.animations[animation_number].used:
-# 		try:
-# 			data.animations[animation_number].gif(f"images/test{hex(animation_number)}.gif", data.palettes['standard'],zoom=2)
-# 		except AssertionError as e:
-# 			print(f"AssertionError on animation {hex(animation_number)}: {e.args}")
-
-data.animations[0x00].gif(f"images/test{hex(0x00)}.gif", data.palettes['standard'],zoom=2)
+for animation_number in range(len(data.animations)):
+	if data.animations[animation_number].used:
+		try:
+			data.animations[animation_number].gif(f"images/test{hex(animation_number)}.gif", data.palettes['standard'],zoom=2)
+		except AssertionError as e:
+			print(f"AssertionError on animation {hex(animation_number)}: {e.args}")
