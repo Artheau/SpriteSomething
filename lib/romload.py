6f14f29fc905aa37a00c63fc555aad2717f243a5
Artheau <isidian@gmail.com>
2019-04-05 08:59:12 -0700

    Implemented gun ports

--- lib/romload.py
+++ lib/romload.py
@@ -5,26 +5,41 @@
 #includes routines that load the rom and apply bugfixes
 #inherits from SNESRomHandler
 
-from RomHandler.rom import RomHandler      #assumes rom.py is in a subfolder called RomHandler
+    
+if __name__ == "__main__":
+    raise AssertionError(f"Called main() on utility library {__file__}")
+else:
+    from .RomHandler.rom import RomHandler      #assumes rom.py is in a subfolder called RomHandler
 
 
 class SamusRomHandler(RomHandler):
     def __init__(self, filename):
         super().__init__(filename)      #do the usual stuff
         self._apply_bugfixes()
+        self._apply_improvements()
 
 
     def get_pose_tilemaps(self,animation,pose):
         lower_tilemaps = self._get_pose_tilemaps_from_addr(0x92945D, animation, pose)
         upper_tilemaps = self._get_pose_tilemaps_from_addr(0x929263, animation, pose)
-        return lower_tilemaps[::-1] + upper_tilemaps[::-1]   #the tilemaps are rendered in this specific order: backwards lower, backwards upper
+
+        #special case here for elevator poses, they have this extra stupid tile
+        #the position is hardcoded into the game at a block starting at $90:868D.
+        #I did not make the code go in there and dig out the offsets because this tile is stupid.
+        if animation in [0x00, 0x9B]:  #elevator poses (power suit/upgraded suit)
+            stupid_tile_tilemap = [[0xF9,0x01,0xF5,0x21,0x38]]   #stupid offsets
+        else:
+            stupid_tile_tilemap = []
+        
+        #the tiles are rendered in this specific order: backwards lower, backwards upper
+        return lower_tilemaps[::-1] + stupid_tile_tilemap + upper_tilemaps[::-1]   
 
 
     def _get_pose_tilemaps_from_addr(self, base_addr, animation, pose):
         #get the pointer to the list of pose pointers (in disassembly: get P??_UT or P??_LT)
-        animation_all_poses_pointer = self.read_from_snes_address(base_addr + 2*animation, 2)
+        animation_all_poses_index = self.read_from_snes_address(base_addr + 2*animation, 2)
         #now get the specific pointer to the tilemap set (in disassembly: get TM_???)
-        pose_tilemaps_pointer = 0x920000 + self.read_from_snes_address(0x92808D + 2*animation_all_poses_pointer, 2)
+        pose_tilemaps_pointer = 0x920000 + self.read_from_snes_address(0x92808D + 2*animation_all_poses_index, 2)
         #now use that pointer to find out how many tilemaps there actually are
         if pose_tilemaps_pointer == 0x920000:    #as will be the case when the pointer is zero, specifying no tiles here
             num_tilemaps = 0
@@ -58,6 +73,105 @@ class SamusRomHandler(RomHandler):
             
         return DMA_writes
 
+    def get_default_vram_data(self, equipped_weapon = "standard"):
+        #go in and get the data that is by default loaded into the VRAM.
+        #Except for stupid tile, this shouldn't be rendered as part of a Samus pose
+        # unless something is glitched out (by this I mean a game glitch, not a bug in my code)
+        # in which case the data here will depend upon what kind of weapon Samus has equipped
+
+        #main population is from data starting at D5200-D71FF LOROM (populates from 0x00 on)
+        #Note: grapple beam (D0200 LOROM) can overwrite parts of row 2, and Mode 7 rooms (183A00 LOROM)
+        # can load "sprites" into the last three rows.  Rain can also go in row 0xD0
+        DMA_writes = {0x00: self.read_from_snes_address(0x9AD200,'1'*0x2000)}
+
+        #row 0x30 is populated with 8 tiles depending upon equipped weapon (0x30-0x37) 
+        if equipped_weapon in ["regular","standard","charge"]:
+            DMA_writes[0x30] = self.read_from_snes_address(0x9AF200,'1'*0x100)
+        elif equipped_weapon in ["ice"]:
+            DMA_writes[0x30] = self.read_from_snes_address(0x9AF400,'1'*0x100)
+        elif equipped_weapon in ["wave"]:
+            DMA_writes[0x30] = self.read_from_snes_address(0x9AF600,'1'*0x100)
+        elif equipped_weapon in ["plasma"]:
+            DMA_writes[0x30] =  self.read_from_snes_address(0x9AF800,'1'*0x100)
+        elif equipped_weapon in ["spazer"]:
+            DMA_writes[0x30] = self.read_from_snes_address(0x9AFA00,'1'*0x100)
+
+        return DMA_writes
+
+
+
+    def get_pose_duration(self,animation, pose):
+        #get the duration list pointer (FD_?? in disassembly)
+        duration_list_location = self._get_duration_list_location(animation)
+        return self.read_from_snes_address(duration_list_location,1)
+
+
+    def get_pose_control_data(self,animation,pose):
+        #similar to get_pose_duration, except that this returns a list containing the pose control code
+        # and its accompanying arguments in a list
+        duration_list_location = self._get_duration_list_location(animation)
+        control_code = self.read_from_snes_address(duration_list_location,1)
+        if control_code in [0xF8,0xFD,0xFE]:
+            return self.read_from_snes_address(duration_list_location, "11")  #these codes have one byte-sized argument
+        elif control_code == 0xF9:
+            return self.read_from_snes_address(duration_list_location, "121111")  #has one word and 4 byte arguments
+        elif control_code == 0xFA:
+            return self.read_from_snes_address(duration_list_location, "111")   #two byte arguments
+        elif control_code == 0xFC:
+            return self.read_from_snes_address(duration_list_location, "1211")    #one word and 2 byte arguments
+        else:
+            return [control_code]
+
+
+    def get_gun_data(self, animation, pose, frame=0x08):
+        #frame is used to tell the code how far the gun port is into the opening process
+        # it opens using three different poses that open over 4 frames each, so full open is 0x08 or more
+
+        #get the pointer to the XY data (XY_P?? in disassembly)
+        cannon_position_pointer = 0x900000 + self.read_from_snes_address(0x90C7DF + 2*animation,2)
+        
+        #first byte tells us the direction the cannon is facing, and if it has high bit set, it only counts for first pose
+        #second byte tells us the rendering priority (0 = do not render, 1 = render in front of Samus, 2 = render behind Samus)
+        direction, priority = self.read_from_snes_address(cannon_position_pointer,"11")
+
+        #if first byte has high bit set and we are not in the zero pose, grab another set of values for use in the following frames 
+        if direction >= 0x80:
+            cannon_position_pointer += 2
+            if pose > 0:
+                direction, priority = self.read_from_snes_address(cannon_position_pointer,"11")
+
+        #TODO: Priority 2 is not implemented in this code -- this should render behind Samus
+
+        if priority in [0x00,0x02]:    #bail out now if the gun should not be drawn
+            tilemap = None
+            DMA_write = None
+        elif priority != 0x01:
+            raise AssertionError(f"get_gun_port() called for data that has non-one priority {priority}")
+        else:
+            direction = direction & 0x7F   #need to clear out that high bit for the rest of this
+
+            #now what is left is a list of xy pairs.  we can just grab what we need.
+            x_position, y_position = self.read_from_snes_address(cannon_position_pointer + 2*(pose+1), "11")
+            
+            tile, palette = self.read_from_snes_address(0x90C791+2*direction, "11")
+
+            #construct the full tilemap
+            x_high_bit = 1 if x_position >= 0x80 else 0
+            tilemap = [x_position, x_high_bit, y_position, tile, palette]
+
+            #need DMA info
+            DMA_list = 0x900000 + self.read_from_snes_address(0x90C7A5 + 2*direction, 2)
+            level_of_opening = min(2, frame//4)
+            DMA_pointer = 0x900000 + self.read_from_snes_address(DMA_list + 2*(1 + level_of_opening), 2)
+
+            DMA_write = {tile: self.read_from_snes_address(DMA_pointer, "1"*0x20)}
+
+        return tilemap, DMA_write
+
+        
+    def _get_duration_list_location(self, animation):
+        return 0x910000 + self.read_from_snes_address(0x91B010 + 2* animation,2)
+
 
     def _apply_single_fix_to_snes_address(self, snes_address, classic_values, fixed_values, encoding):
         #checks to see if, indeed, a value is still in the classic (bugged) value, and if so, fixes it
@@ -74,7 +188,10 @@ class SamusRomHandler(RomHandler):
             return False
 
 
-    def _apply_bugfixes(self):
+    def _apply_improvements(self):
+        #these are not mandatory for the animation viewer to work, but they are general quality of life improvements
+        # that I recommend to make.
+
         '''
         ;E508
         AFP_T31:;Midair morphball facing right without springball
@@ -83,7 +200,6 @@ class SamusRomHandler(RomHandler):
         #this bug preventing left and right morphball from being different, but now we fix this
 
         self._apply_single_fix_to_snes_address(0x92D9B2,0xE508,0xE530,2)
-
 
         '''
         ;$B361
@@ -100,6 +216,20 @@ class SamusRomHandler(RomHandler):
         fixed_values    = [0x02,0x10,0x10,0xFE,0x01]
         self._apply_single_fix_to_snes_address(0x91B361,original_values,fixed_values,"11111")
 
+        '''
+        ;c9db
+        XY_P00:     ;00:;Facing forward, ala Elevator pose (power suit)
+        XY_P9B:     ;9B:;Facing forward, ala Elevator pose (Varia and/or Gravity Suit)
+        DB $00, $02 
+        '''
+        #the second byte here is supposed to be 00, but because it is not, the missile port is rendered behind Samus's left
+        # fist during elevator pose.
+        self._apply_single_fix_to_snes_address(0x90c9db,[0x00,0x02],[0x00,0x00],"11")
+
+
+
+    def _apply_bugfixes(self):
+        #these are significant typos that reference bad palettes or similar, and would raise assertion errors in any clean code
 
         '''
         TM_193:
@@ -143,11 +273,3 @@ class SamusRomHandler(RomHandler):
         original_values = [0xF8,0x01,0xF8,0x00,0x30]
         fixed_values    = [0xF8,0x01,0xF8,0x00,0x38]
         self._apply_single_fix_to_snes_address(0x92A12E,original_values,fixed_values,"11111")
-
-
-
-def main():
-    print(f"Called main() on utility library {__file__}")
-    
-if __name__ == "__main__":
-    main()
