d6cc45c99fe75d9b149c7e25281449590d088ab8
Mike A. Trethewey <miketrethewey@gmail.com>
2019-07-31 03:25:31 -0700

    upx, less verbose

--- scripts/get_upx.sh
+++ scripts/get_upx.sh
@@ -14,40 +14,23 @@ mkdir -p $HOME/${TRAVIS_REPO_SLUG}/upx
 #different archiving utilities
 if [ "${TRAVIS_OS_NAME}" == "windows" ]; then
 	#curl seems to fail on windows builds
-	echo "Getting UPX"
 	wget --output-document=${UPX_FILE} ${UPX_URL}
-	echo "Got UPX"
 
 	#use archiver that we installed before getting here
-	echo "Extracting UPX"
 	arc unarchive ${UPX_FILE} $HOME/upx
-	echo "Extracted UPX"
 
 	#move upx to where pyinstaller expects it to be
-	echo "Moving UPX"
 	mv $HOME/upx/${UPX_SLUG}/* $HOME/${TRAVIS_REPO_SLUG}/upx
 	echo "Moved UPX"
 
-	#delete the folder we extracted it to initially
-	rm -r $HOME/upx
-
-	ls $HOME/${TRAVIS_REPO_SLUG}/upx
+	ls -p $HOME/${TRAVIS_REPO_SLUG}/upx
 else
-	echo "Getting UPX"
 	curl -L ${UPX_URL} --output ${UPX_FILE}
-	echo "Got UPX"
-
-	echo "Extracting UPX"
 	tar -C $HOME/upx -xf ${UPX_FILE}
-	echo "Extracted UPX"
 
 	#move upx to where pyinstaller expects it to be
-	echo "Moving UPX"
 	mv $HOME/upx/${UPX_SLUG}/* $HOME/${TRAVIS_REPO_SLUG}/upx
 	echo "Moved UPX"
 
-	#delete the folder we extracted it to initially
-	rm -r $HOME/upx
-
-	ls $HOME/${TRAVIS_REPO_SLUG}/upx
+	ls -p $HOME/${TRAVIS_REPO_SLUG}/upx
 fi
