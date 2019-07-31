afade2b23dd9086aadba2e2c877a06ae92ea69bc
Mike A. Trethewey <miketrethewey@gmail.com>
2019-07-31 01:00:55 -0700

    Factor in some more Win stuff

--- scripts/win-before_install.sh
+++ scripts/win-before_install.sh
@@ -2,12 +2,5 @@
 
 set -ev
 
-#get pip getter
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-
-#get pip
-${PYTHON_EXECUTABLE} get-pip.py
-${PYTHON_EXECUTABLE} -m pip install --upgrade pip
-
 #get archive utility
 choco install archiver
