b93bb91e46da7525b477572541a0fb742fcd6c85
Mike A. Trethewey <miketrethewey@gmail.com>
2019-07-31 00:50:39 -0700

    Factor in some Win stuff
    Thought I could be efficient. Guess not.

--- scripts/win-before_install.sh
+++ scripts/win-before_install.sh
@@ -2,18 +2,12 @@
 
 set -ev
 
-#get archive utility
-choco install archiver
-
-#get python
-choco install python
-
-#export path of python
-export PATH=/c/Python37:/c/Python37/Scripts:$PATH
-
 #get pip getter
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
 #get pip
 ${PYTHON_EXECUTABLE} get-pip.py
 ${PYTHON_EXECUTABLE} -m pip install --upgrade pip
+
+#get archive utility
+choco install archiver
