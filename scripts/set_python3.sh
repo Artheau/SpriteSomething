3cd1dbd74b1994044f2cb2f03b2b1b1a2a370607
Mike A. Trethewey <miketrethewey@gmail.com>
2019-07-30 23:37:11 -0700

    pip maintenance
    Upgrade pip
    pip3 version
    pip3 installs wheel

---
+++ scripts/set_python3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -ev
+
+export PYTHON_EXECUTABLE=python3
--- scripts/win-before_install.sh
+++ scripts/win-before_install.sh
@@ -4,13 +4,16 @@ set -ev
 
 #get archive utility
 choco install archiver
+
 #get python
 choco install python
+
 #export path of python
+./scripts/set_python3.sh
 export PATH=/c/Python37:/c/Python37/Scripts:$PATH
+
 #get pip getter
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
 #get pip
 ${PYTHON_EXECUTABLE} get-pip.py
-#upgrade pip
-${PYTHON_EXECUTABLE} -m pip install --upgrade pip
