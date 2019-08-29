ff94530078cacc7b20c3e2246723f166d1d10abe
Mike A. Trethewey <miketrethewey@gmail.com>
2019-08-29 00:20:37 -0700

    Fix Pages script
    `after_install` doesn't exist, use `after_success` instead
    Pages provider nukes all of it, ensure that our other resources still exist
    Move GHPages resources to staging area, remove from source code distribution

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -13,6 +13,11 @@ mkdir ../pages/app_resources/meta
 mkdir ../pages/app_resources/meta/manifests
 echo "${TRAVIS_TAG}" > "../pages/app_resources/meta/manifests/app_version.txt"
 
+#move GitHub pages files to staging area
+mv ./pages_resources/* ../pages/app_resources
+#nuke GitHub pages files from source code
+rm -rf ./pages_resources
+
 #chmod user_resources to hopefully fix working_dirs.json issue
 chmod 775 "./user_resources"
 chmod 775 "./user_resources/meta"
