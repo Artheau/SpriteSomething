25ebf22526e2fd853a186ba1c6e2017efc488719
Mike A. Trethewey <miketrethewey@gmail.com>
2019-09-14 01:40:13 -0700

    pip/pip3

--- scripts/travis/install.py
+++ scripts/travis/install.py
@@ -1,4 +1,10 @@
+import os
 import subprocess
 
-subprocess.check_call("pip --version")
-subprocess.check_call("pip install -r ./app_resources/meta/manifests/pip_requirements.txt")
+TRAVIS_OS_NAME = os.environ.get("TRAVIS_OS_NAME") or "TRAVIS_OS_NAME"
+PIP_EXECUTABLE = "pip3"
+if TRAVIS_OS_NAME == "windows":
+	PIP_EXECUTABLE = "pip"
+
+subprocess.check_call(PIP_EXECUTABLE + " --version")
+subprocess.check_call(PIP_EXECUTABLE + " install -r ./app_resources/meta/manifests/pip_requirements.txt")
