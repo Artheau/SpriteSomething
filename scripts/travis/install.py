c0439105a32b2c07859544d76fa42d913af3e7ab
Mike A. Trethewey <miketrethewey@gmail.com>
2019-09-14 01:48:37 -0700

    Simplify pip

--- scripts/travis/install.py
+++ scripts/travis/install.py
@@ -1,10 +1,4 @@
-import os
 import subprocess
 
-TRAVIS_OS_NAME = os.environ.get("TRAVIS_OS_NAME") or "TRAVIS_OS_NAME"
-PIP_EXECUTABLE = "pip3"
-if TRAVIS_OS_NAME == "windows":
-	PIP_EXECUTABLE = "pip"
-
 subprocess.check_call("pip --version")
-subprocess.check_call(PIP_EXECUTABLE + " install -r ./app_resources/meta/manifests/pip_requirements.txt")
+subprocess.check_call("pip install -r ./app_resources/meta/manifests/pip_requirements.txt")
