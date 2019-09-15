1c21b037c9f4173c1476608e659e0d3a8a632c91
Mike A. Trethewey <miketrethewey@gmail.com>
2019-09-14 21:02:50 -0700

    Spiff up pip

--- scripts/travis/install.py
+++ scripts/travis/install.py
@@ -2,9 +2,10 @@ import os
 import subprocess
 
 TRAVIS_OS_NAME = os.environ.get("TRAVIS_OS_NAME") or "TRAVIS_OS_NAME"
-PIP_EXECUTABLE = "pip3"
-if TRAVIS_OS_NAME == "windows":
-	PIP_EXECUTABLE = "pip"
+PYTHON_EXECUTABLE = "python3" if TRAVIS_OS_NAME == "osx" else "python"
+PIP_EXECUTABLE = "pip" if TRAVIS_OS_NAME == "windows" else "pip3"
+
+subprocess.check_call([PYTHON_EXECUTABLE,"-m","pip","install","--upgrade","pip"])
 
 subprocess.check_call([PIP_EXECUTABLE,"--version"])
 if PIP_EXECUTABLE == "pip3":
