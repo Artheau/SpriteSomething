fb0ca775606f01838a3b54aaffa87dcb22255c06
Mike A. Trethewey <miketrethewey@gmail.com>
2019-10-05 22:55:19 -0700

    Write tag to appversion

--- resources/ci/common/prepare_appversion.py
+++ resources/ci/common/prepare_appversion.py
@@ -2,15 +2,10 @@ import common
 import os               # for env vars
 from shutil import copy	# file manipulation
 
-# get app version
-APP_VERSION = ""
-with open("./resources/app/meta/manifests/app_version.txt","r+") as f:
-	APP_VERSION = f.readlines()[0].strip()
-
 env = common.prepare_env()
 
 # set tag to app_version.txt
-with open("./resources/app/meta/manifests/app_version.txt","r+") as f:
+with open("./resources/app/meta/manifests/app_version.txt","w") as f:
 	_ = f.read()
 	f.seek(0)
 	f.write(env["GITHUB_TAG"])
