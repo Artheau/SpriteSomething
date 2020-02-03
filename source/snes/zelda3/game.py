6a25daff5df168a36f0524da6f1d0137b3423e6d
Mike A. Trethewey <miketrethewey@gmail.com>
2020-02-02 22:07:33 -0800

    Move all the things
    This is a broken state

---
+++ source/snes/zelda3/game.py
@@ -0,0 +1,9 @@
+from source.meta.gui.gamelib import GameParent
+
+class Game(GameParent):
+	def __init__(self):
+		super().__init__()
+		#FIXME: Do we want to translate the game's display name?
+		self.name = "The Legend of Zelda: A Link to the Past"
+		self.internal_name = "zelda3"
+		self.load_plugins()
