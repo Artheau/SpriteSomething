35992a732776fa769e547efaf7177c83b5789444
Mike A. Trethewey <miketrethewey@gmail.com>
2020-03-28 00:14:30 -0700

    Update Sprite Listings

--- source/pc/spelunky/player/plugins.py
+++ source/pc/spelunky/player/plugins.py
@@ -5,7 +5,6 @@ from functools import partial
 from source.meta.common import common
 from source.meta.gui import gui_common
 from source.meta.classes.pluginslib import PluginsParent
-from . import equipment
 
 #FIXME: English
 class Plugins(PluginsParent):
@@ -13,18 +12,14 @@ class Plugins(PluginsParent):
 		super().__init__()
 		plugins = [
 			("Download SpriteSomething Unofficial Sprites",None,self.get_spritesomething_sprites)
-#			("Equipment",None,partial(self.equipment_test,True))
 		]
 		self.set_plugins(plugins)
-
-	def equipment_test(self, save=False):
-		return equipment.equipment_test(save)
 
 	def get_spritesomething_sprites(self):
 		success = gui_common.get_sprites(
 			self,
-			"Unofficial SpriteSomething Samus",
-			"snes/metroid3/samus/sheets/unofficial",
-			"https://raw.githubusercontent.com/Artheau/SpriteSomething/gh-pages/resources/app/snes/metroid3/samus/sprites.json"
+			"Unofficial SpriteSomething Spelunky Player",
+			"pc/spelunky/player/sheets/unofficial",
+			"https://raw.githubusercontent.com/Artheau/SpriteSomething/gh-pages/resources/app/pc/spelunky/player/sprites.json"
 		)
 		return success
