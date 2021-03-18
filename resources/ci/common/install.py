7e21285727146417b0288d038635cc53f9a5f2a5
Mike A. Trethewey <miketrethewey@gmail.com>
2021-03-18 00:05:35 -0700

    Nobody saw that...

--- resources/ci/common/install.py
+++ resources/ci/common/install.py
@@ -1,63 +1,94 @@
 import common
 import argparse
 import os
-import urllib.request, ssl
+import platform
 import subprocess # do stuff at the shell level
 
 env = common.prepare_env()
 
-def get_get_pip(PY_VERSION):
-  try:
-    import pip
-  except ImportError:
-    print("Getting pip getter!")
-    #make the request!
-    url = "https://bootstrap.pypa.io/get-pip.py"
-    context = ssl._create_unverified_context()
-    req = urllib.request.urlopen(url, context=context)
-    got_pip = req.read().decode("utf-8")
+pip_requirements = os.path.join(".","resources","app","meta","manifests","pip_requirements.txt")
 
-    with open("get-pip.py", "w") as g:
-      req = urllib.request.Request(
-        url,
-        data=None,
-        headers={
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
-        }
-      )
-      req = urllib.request.urlopen(req, context=context)
-      data = req.read().decode("utf-8")
-      g.write(data)
+def run_install(PY_VERSION,USER):
+  # get executables
+  #  python
+  #   linux/windows: python
+  #   macosx:        python3
+  #  pip
+  #   linux/macosx: pip3
+  #   windows:      pip
+  PYTHON_PATH = env["PYTHON_EXE_PATH"]
+  PYTHON_EXECUTABLE = "python3" if "osx" in env["OS_NAME"] else "python"
+  PIP_PATH = env["PIP_EXE_PATH"]
+  PIP_EXECUTABLE = "pip" if "windows" in env["OS_NAME"] else "pip3"
+  PIP_EXECUTABLE = "pip" if "osx" in env["OS_NAME"] and "actions" in env["CI_SYSTEM"] else PIP_EXECUTABLE
 
-    # get executables
-    #  python
-    #   linux/windows: python
-    #   macosx:        python3
-    PYTHON_EXECUTABLE = "python3" if "osx" in env["OS_NAME"] else "python"
-    if PY_VERSION == None:
-      PY_VERSION = 0
+  if PY_VERSION == None:
+    PY_VERSION = 0
+  if USER == None:
+    USER = False
 
-    if float(PY_VERSION) > 0:
-      PYTHON_EXECUTABLE = "py"
+  if float(PY_VERSION) > 0:
+    PYTHON_EXECUTABLE = "py"
+    PYTHON_PATH = env["PY_EXE_PATH"]
+    print("Installing to Python %.1f via Py Launcher" % float(PY_VERSION))
+  else:
+    print("Installing to Python %s" % platform.python_version())
+  print("Installing packages at %s level" % ("User" if USER else "Global"))
 
-    print("Getting pip!")
+  print()
+  print("Upgrading pip-")
+  # upgrade pip
+  args = [
+    PYTHON_PATH + PYTHON_EXECUTABLE,
+    '-' + str(PY_VERSION),
+    "-m",
+    "pip",
+    "install",
+    "--upgrade",
+    "--user",
+    "pip"
+  ]
+  if not USER:
+    args.remove("--user")
+  if PY_VERSION == 0:
+    del args[1]
+  subprocess.check_call(args)
+
+  # if pip3, install wheel
+  if PIP_EXECUTABLE == "pip3":
+    print("Installing Wheel!")
     args = [
-      env["PYTHON_EXE_PATH"] + PYTHON_EXECUTABLE,
-      '-' + str(PY_VERSION),
-      "get-pip.py"
+      PIP_PATH + PIP_EXECUTABLE,
+      "install",
+      "--user",
+      "-U",
+      "wheel"
     ]
-    if PY_VERSION == 0:
-      del args[1]
+    if not USER:
+      args.remove("--user")
     subprocess.check_call(args)
+
+  print()
+  # install listed dependencies
+  print("Installing dependencies")
+  print("-----------------------")
+  args = [
+    PIP_PATH + PIP_EXECUTABLE,
+    "install",
+    "--user",
+    "-r",
+    pip_requirements
+  ]
+  if not USER:
+    args.remove("--user")
+  subprocess.check_call(args)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(add_help=False)
   parser.add_argument('--py', default=0)
+  parser.add_argument('--user', default=False, action="store_true")
   command_line_args = parser.parse_args()
   PY_VERSION = vars(command_line_args)["py"]
+  USER = vars(command_line_args)["user"]
 
-  try:
-    import pip
-    print("pip is installed")
-  except ImportError:
-    get_get_pip(PY_VERSION)
+  run_install(PY_VERSION,USER)
