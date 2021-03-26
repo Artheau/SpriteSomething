123ff492cdea4988b7f66eec2b224bd17e69ceab
Mike A. Trethewey <miketrethewey@gmail.com>
2021-03-26 03:25:52 -0700

    Update CI

--- source/meta/build.py
+++ source/meta/build.py
@@ -1,5 +1,11 @@
+import argparse
+import platform
 import subprocess	#for executing scripts within scripts
 import os					#for checking for dirs
+
+from resources.ci.common import common
+
+env = common.prepare_env()
 
 DEST_DIRECTORY = "."
 
@@ -10,10 +16,40 @@ if os.path.isdir("upx"):
 else:
 	upx_string = ""
 
-subprocess.run(" ".join(["pyinstaller ./source/SpriteSomething.spec ",
-						 upx_string,
-						 "-y ",                #overwrite dist directory if necessary
-						 "--onefile "          #compile everything into a single file, except for resources and whatnot
-						 f"--distpath {DEST_DIRECTORY} ",      #place the executable in the specified directory
-						 ])
-						)
+def run_build(PY_VERSION):
+  # PYTHON_PATH = env["PYTHON_EXE_PATH"]
+  # PYTHON_EXECUTABLE = "python3" if "osx" in env["OS_NAME"] else "python"
+
+  # if PY_VERSION is None:
+  #   PY_VERSION = 0
+
+  # if float(PY_VERSION) > 0:
+  #   PYTHON_EXECUTABLE = "py"
+  #   PYTHON_PATH = env["PY_EXE_PATH"]
+  #   print("Building via Python %.1f via Py Launcher" % float(PY_VERSION))
+  # else:
+  #   print("Building via Python %s" % platform.python_version())
+
+  print("Building via Python %s" % platform.python_version())
+
+  PYINST_EXECUTABLE = "pyinstaller"
+  args = [
+    "source/SpriteSomething.spec",
+    upx_string,
+    # "-y ",
+    "--onefile ",
+    f"--distpath {DEST_DIRECTORY}"
+  ]
+  # if PY_VERSION == 0:
+  #   del args[1]
+  subprocess.check_call([PYINST_EXECUTABLE,*args])
+
+if __name__ == "__main__":
+  # parser = argparse.ArgumentParser(add_help=False)
+  # parser.add_argument('--py', default=0)
+  # command_line_args = parser.parse_args()
+  # PY_VERSION = vars(command_line_args)["py"]
+
+  PY_VERSION = 0
+
+  run_build(PY_VERSION)
