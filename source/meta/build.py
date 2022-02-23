d06e39c93124b35c03aba5c0991749228ea1b8c1
Mike A. Trethewey <miketrethewey@gmail.com>
2022-02-22 19:12:19 -0800

    Try new builder

--- source/meta/build.py
+++ source/meta/build.py
@@ -1,30 +1,41 @@
-import argparse
 import platform
 import subprocess	#for executing scripts within scripts
 import os					#for checking for dirs
+import sys
+from shutil import move
 
 DEST_DIRECTORY = "."
 
-#UPX greatly reduces the filesize.  You can get this utility from https://upx.github.io/
-#just place it in a subdirectory named "upx" and this script will find it
-if os.path.isdir("./upx"):
-	upx_string = "--upx-dir=upx "
-else:
-	upx_string = ""
-
 def run_build(PY_VERSION):
   print("Building via Python %s" % platform.python_version())
+  include_modules = []
 
-  PYINST_EXECUTABLE = "pyinstaller"
+  for (r,d,f) in os.walk(os.path.join(".","source")):
+    if "__pycache__" not in r:
+      if "__pycache__" in d:
+        d.remove("__pycache__")
+      if "__init__.py" in f:
+        f.remove("__init__.py")
+      for fName in f:
+        if ".py" in fName:
+          include_modules.append(r.replace(os.sep,'.').replace("..",'') + '.' + fName.replace(".py",''))
+
+  DISTILLER_EXECUTABLE = "cxfreeze"
   args = [
-    "source/SpriteSomething.spec",
-    upx_string,
-    "-y",
-    "--onefile",
-    f"--distpath={DEST_DIRECTORY}"
+    "SpriteSomething.py",
+    "--include-modules=" + ",".join(include_modules)
   ]
-  print("PyInstaller args: %s" % " ".join(args))
-  subprocess.check_call([PYINST_EXECUTABLE,*args])
+  print("Distiller args: %s" % " ".join(args))
+  subprocess.check_call([DISTILLER_EXECUTABLE,*args])
+
+  if "win32" in sys.platform:
+    for f in os.listdir(os.path.join(".","build")):
+      for g in os.listdir(os.path.join(".","build",f)):
+        move(
+          os.path.join(".","build",f,g),
+          os.path.join(".")
+        )
+
 
 if __name__ == "__main__":
   PY_VERSION = 0
