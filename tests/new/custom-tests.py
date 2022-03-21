a6fd1c3f20be8fd35cd28a7595e4b1e82c8c517a
Mike A. Trethewey <miketrethewey@gmail.com>
2022-03-21 14:15:52 -0700

    Clean up tests

--- tests/new/custom-tests.py
+++ tests/new/custom-tests.py
@@ -1,16 +1,16 @@
-from common import DATA  # get pathdata
-from resources.ci.common import my_path
-from resources.ci.common import common
-import importlib  # dynamic sprite imports
-import unittest  # tests
-import json  # parsing JSON objects
-import os  # os pathing
+from common import DATA # get pathdata
+import importlib        # dynamic sprite imports
+import unittest         # tests
+import json             # parsing JSON objects
+import os               # os pathing
 import platform
 import subprocess
 import sys
-import tempfile  # temp for objects
+import tempfile         # temp for objects
 import traceback
 
+global VERBOSE
+VERBOSE = False
 
 try:
     from PIL import ImageChops
@@ -21,33 +21,25 @@ except ModuleNotFoundError as e:
     except ModuleNotFoundError as e:
         print(e)
 
-env = common.prepare_env()  # get environment variables
-WIDTH = 70  # width for labels
-
-
-args = []
-PIPEXE = ""
-PYTHON_EXECUTABLE = os.path.splitext(sys.executable.split(os.path.sep).pop())[
-    0]  # get command to run python
-# get python version
-PYTHON_VERSION = sys.version.split(" ")[0]
-# get python major.minor version
-PYTHON_MINOR_VERSION = '.'.join(PYTHON_VERSION.split(".")[:2])
-PIP_VERSION = ""
-PIP_FLOAT_VERSION = 0
-VERBOSE = True
-VERSIONS = {}
-
-
 def get_module_version(module):
     # pip index versions [module]                             // >= 21.2
     # pip install [module]==                                  // >= 21.1
     # pip install --use-deprecated=legacy-resolver [module]== // >= 20.3
     # pip install [module]==                                  // >=  9.0
     # pip install [module]==blork                             // <   9.0
-    global args
-    global PIPEXE
-    global PIP_FLOAT_VERSION
+    args = ""
+    PIPEXE = ""
+    PIP_FLOAT_VERSION = ""
+
+    if args == "" or \
+      PIPEXE == "" or \
+      PIP_FLOAT_VERSION == "":
+      with open(os.path.join(".","resources","user","meta","manifests","settings.json"),"r") as settingsFile:
+        settings = json.load(settingsFile)
+        args = settings["py"]
+        PIPEXE = settings["pip"]
+        PIP_FLOAT_VERSION = settings["versions"]["pip"]["version"][1]
+
     ret = ""
     ver = ""
 
@@ -76,68 +68,6 @@ def get_module_version(module):
     #     ver = (ret.stderr.strip().split("\n")[0].split(",")[-1].replace(')', '')).strip()
 
     return ver
-
-
-def do_python():
-    # get python debug info
-    ret = subprocess.run([*args, "--version"], capture_output=True, text=True)
-    if ret.stdout.strip():
-        PYTHON_VERSION = ret.stdout.strip().split(" ")[1]
-        PY_STRING = (
-            "%s\t%s\t%s"
-            %
-            (
-                ((isinstance(args[0], list) and " ".join(
-                  args[0])) or args[0]).strip(),
-                PYTHON_VERSION,
-                sys.platform
-            )
-        )
-        print(PY_STRING)
-        print('.' * WIDTH)
-
-
-def do_pip():
-    global VERSIONS
-    # get pip debug info
-    ret = subprocess.run([*args, "-m", PIPEXE, "--version"],
-                         capture_output=True, text=True)
-    if ret.stdout.strip():
-        if " from " in ret.stdout.strip():
-            PIP_VERSION = ret.stdout.strip().split(" from ")[
-                0].split(" ")[1]
-            if PIP_VERSION:
-                b, f, a = PIP_VERSION.partition('.')
-                global PIP_FLOAT_VERSION
-                PIP_FLOAT_VERSION = b+f+a.replace('.', '')
-                PIP_LATEST = get_module_version("pip")
-
-                VERSIONS["py"] = {"version": PYTHON_VERSION,
-                                  "platform": sys.platform}
-                VERSIONS["pip"] = {
-                    "version": [
-                        PIP_VERSION,
-                        PIP_FLOAT_VERSION
-                    ],
-                    "latest": PIP_LATEST
-                }
-
-                PIP_STRING = (
-                    "%s\t%s\t%s\t%s\t%s\t%s"
-                    %
-                    (
-                        ((isinstance(args[0], list) and " ".join(
-                            args[0])) or args[0]).strip(),
-                        PYTHON_VERSION,
-                        sys.platform,
-                        PIP_EXECUTABLE,
-                        PIP_VERSION,
-                        PIP_LATEST
-                    )
-                )
-                print(PIP_STRING)
-                print('.' * WIDTH)
-
 
 class ExportAudit(unittest.TestCase):
     def __init__(self, platID, gameID, spriteID):
@@ -224,58 +154,9 @@ class ExportAudit(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    # figure out pip executable
-    PIP_EXECUTABLE = "pip" if "windows" in env["OS_NAME"] else "pip3"
-    PIP_EXECUTABLE = "pip" if "osx" in env["OS_NAME"] and "actions" in env["CI_SYSTEM"] else PIP_EXECUTABLE
-
-    PIP_VERSION = ""  # holder for pip's version
-
-    success = False
-    # foreach py executable
-    for PYEXE in ["py", "python3", "python"]:
-        if success:
-            continue
-
-        args = []
-        # if it's the py launcher, specify the version
-        if PYEXE == "py":
-            PYEXE = [PYEXE, "-" + PYTHON_MINOR_VERSION]
-            # if it ain't windows, skip it
-            if "windows" not in env["OS_NAME"]:
-                continue
-
-        # build executable command
-        if isinstance(PYEXE, list):
-            args = [*PYEXE]
-        else:
-            args = [PYEXE]
-
-        try:
-            do_python()  # print python debug data
-
-            # foreach py executable
-            for PIPEXE in ["pip3", "pip"]:
-                do_pip()        # print pip debug data
-                # upgrade pip
-                ret = subprocess.run(
-                    [*args, "-m", PIPEXE, "install", "--upgrade", "pip"], capture_output=True, text=True)
-                # get output
-                if ret.stdout.strip():
-                    # if it's not already satisfied, update it
-                    if "already satisfied" not in ret.stdout.strip():
-                        print(ret.stdout.strip())
-                        do_pip()
-                        success = True
-                else:
-                    success = True
-
-        # if something else went fucky, print it
-        except Exception as e:
-            traceback.print_exc()
-
     module = "pillow"
     print("%s\t%s" % (module, get_module_version("pillow")))
-    print('.' * WIDTH)
+    print('.' * 70)
 
     for [platID, plat] in DATA.items():
         for [gameID, game] in plat["games"].items():
