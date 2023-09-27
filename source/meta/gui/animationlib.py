ad343f55bef6b058524d1d4a80cd6751574dcd03
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2023-09-26 22:39:43 -0700

    Fix anim lib again

--- source/meta/gui/animationlib.py
+++ source/meta/gui/animationlib.py
@@ -274,14 +274,14 @@ class AnimationEngineParent():
   def get_spiffy_buttons(self, parent, fish):
     spiffy_buttons = widgetlib.SpiffyButtons(parent, self.resource_subpath, self)
 
-		spiffy_manifest = common.get_resource([self.resource_subpath,"manifests"],"spiffy-buttons.json")
-		if spiffy_manifest:
-			with open(spiffy_manifest) as f:
-				spiffy_list = {}
-				try:
-					spiffy_list = json.load(f)
-				except JSONDecodeError as e:
-					raise ValueError("Spiffy Buttons Manifest malformed: " + self.game.internal_name + "/" + self.sprite.internal_name)
+    spiffy_manifest = common.get_resource([self.resource_subpath,"manifests"],"spiffy-buttons.json")
+    if spiffy_manifest:
+        with open(spiffy_manifest) as f:
+            spiffy_list = {}
+            try:
+                spiffy_list = json.load(f)
+            except JSONDecodeError as e:
+                raise ValueError("Spiffy Buttons Manifest malformed: " + self.game.internal_name + "/" + self.sprite.internal_name)
 
         for group in spiffy_list["button-groups"]:
           if len(group["buttons"]):
@@ -309,11 +309,11 @@ class AnimationEngineParent():
     direction_manifest = common.get_resource([self.resource_subpath,"manifests"],"direction-buttons.json")
     if direction_manifest:
       with open(direction_manifest) as f:
-				direction_list = {}
-				try:
-	        direction_list = json.load(f)
-				except JSONDecodeError as e:
-					raise ValueError("Direction Buttons Manifest malformed: " + self.game.internal_name + "/" + self.sprite.internal_name)
+        direction_list = {}
+        try:
+          direction_list = json.load(f)
+        except JSONDecodeError as e:
+          raise ValueError("Direction Buttons Manifest malformed: " + self.game.internal_name + "/" + self.sprite.internal_name)
         for group in direction_list["button-groups"]:
           if len(group["buttons"]):
             group_key = group["group-fish-key"]
